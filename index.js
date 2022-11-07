2a6ef52d22d681180dc503448fa0cc2a366b26cf
chengying <hollye2558@gmail.com>
2022-11-07 15:38:58 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(array,name) {
+   return array.filter (element => element.toLowerCase() === name.toLowerCase());
+}
+ 
+
+function fuzzyMatch(array,query) { 
+    return array.filter (element => element[0].toLowerCase().includes(query[0].toLowerCase()));
+}
+
+function matchName (array,name) {
+    return array.filter (element => element.name === name);
+}
